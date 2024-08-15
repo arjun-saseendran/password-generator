187e19d688b8cf5c0efe30dead7c63f994a77d57
arjun-saseendran <arjunsaseendran@icloud.com>
2024-08-15 18:45:47 +0530

    add states length, number allowed, char allowed, password

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import { useState } from "react"
+
 
 function App() {
-  
+const [length, setLength] = useState(8);
+const [numberAllowed, setNumberAllowed] = useState(false);
+const [charAllowed, setCharAllowed] = useState(false);
+const [password, setPassword] = useState('');
 
-  return (
+return (
     <>
-      
+      <h1>password genarator</h1>
     </>
   )
 }
